d609077088ba18e3ab31ccf4273a215ad67c182a
iremyux <iremyuksel@microsoft.com>
2026-02-06 16:39:44 +0100

    Update handle

--- src/libraries/Common/src/Microsoft/Win32/SafeHandles/SafeLzmaHandle.cs
+++ src/libraries/Common/src/Microsoft/Win32/SafeHandles/SafeLzmaHandle.cs
@@ -3,20 +3,49 @@
 
 using System;
 using System.IO.Compression;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 namespace Microsoft.Win32.SafeHandles
 {
     internal sealed class SafeLzmaHandle : SafeHandle
     {
-        public SafeLzmaHandle() : base(IntPtr.Zero, true) { }
+        private LzmaNative.LzmaStream _stream;
+
+        public SafeLzmaHandle() : base(IntPtr.Zero, true)
+        {
+            // Initialize the stream to zero
+            _stream = default;
+            // Use 1 as a sentinel value to indicate the handle is valid
+            // The actual state is in _stream
+            SetHandle((IntPtr)1);
+        }
 
         protected override bool ReleaseHandle()
         {
-            Interop.Lzma.lzma_end((LzmaNative.LzmaStream*)handle);
+            unsafe
+            {
+                fixed (LzmaNative.LzmaStream* strm = &_stream)
+                {
+                    Interop.Lzma.lzma_end(strm);
+                }
+            }
             return true;
         }
 
         public override bool IsInvalid => handle == IntPtr.Zero;
+
+        /// <summary>
+        /// Gets a reference to the underlying LzmaStream structure.
+        /// </summary>
+        public ref LzmaNative.LzmaStream GetStream() => ref _stream;
+
+        /// <summary>
+        /// Gets an unsafe pointer to the LzmaStream structure.
+        /// </summary>
+        public unsafe LzmaNative.LzmaStream* GetStreamPointer()
+        {
+            return (LzmaNative.LzmaStream*)Unsafe.AsPointer(ref _stream);
+        }
     }
 }
